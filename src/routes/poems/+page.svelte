2dc2eb5f6e71bed0ce305a002c55601b9e80a077
Ben Judson <ben@shiftingedges.com>
2024-04-07 09:25:53 -0500

    add poems index with random redirect

---
+++ src/routes/poems/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from "svelte";
+
+  const poems = [
+    "a-bird-was-given-the-heart",
+    "after-all-the-river-could-swallow-us",
+    "because-of-the-eye-the-hand-fell",
+    "fire-leaves-its-soul-where-it-steps",
+    "i-dreamed-that",
+    "long-strands-of-light",
+    "scratching-the-moon-into-earth",
+    "the-bird-slowly-grew-a-fear",
+    "the-light-does-not-know",
+    "the-weight-of-your-eyelids",
+    "when-there-is-space-we-will-meet",
+    "your-breath-has-buried",
+  ];
+
+  onMount(() => {
+    window.location.href = `poems/${poems[Math.floor(Math.random() * poems.length)]}`;
+  });
+</script>
