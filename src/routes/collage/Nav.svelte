4241f94b2d2cf06fa3f8ef130c2a6bb01ab176d6
Ben Judson <ben@shiftingedges.com>
2024-04-15 22:14:05 -0500

    add collage nav component with styles

---
+++ src/routes/collage/Nav.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let visiblePages: string[] = [];
+  export let currentPage: string = "";
+</script>
+
+<nav>
+  {#each visiblePages as page}
+    {#if page === currentPage}
+      <span>{page.replace(/-/g, " ")}</span>
+    {:else}
+      <a href="/collage/{page}">
+        {page.replace(/-/g, " ")}
+      </a>
+    {/if}
+  {/each}
+</nav>
+
+<style>
+  nav {
+    width: 100%;
+    max-width: 600px;
+  }
+
+  nav a {
+    display: inline-block;
+    border: 1px dotted var(--fg-color);
+    padding: 0.25rem 0.5rem;
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+    text-decoration: none;
+    white-space: nowrap;
+    color: #fe5f55;
+  }
+
+  nav span {
+    display: inline-block;
+    padding: 0.25rem 0.5rem;
+    margin-right: 0.5rem;
+    color: #a5a5a5;
+  }
+</style>
--- src/routes/collage/TopNav.svelte
+++ src/routes/collage/TopNav.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-	export let currentPage: string;
-	export let pages: string[] = [];
+  import Nav from "./Nav.svelte";
 
-	$: currentIndex = pages.findIndex((page) => page === currentPage);
-	$: visiblePages = pages.slice(0, currentIndex > -1 ? currentIndex + 1 : pages.length - 1);
+  export let currentPage: string;
+  export let pages: string[] = [];
+
+  $: currentIndex = pages.findIndex((page) => page === currentPage);
+  $: visiblePages = pages.slice(
+    0,
+    currentIndex > -1 ? currentIndex + 1 : pages.length - 1
+  );
 </script>
 
-<nav>
-	{#each visiblePages as page}
-		{#if page === currentPage}
-			<span>{page.replace(/-/g, ' ')}</span>
-		{:else}
-			<a href="/collage/{page}">
-				{page.replace(/-/g, ' ')}
-			</a>
-		{/if}
-	{/each}
-</nav>
+<Nav {visiblePages} {currentPage} />
